08e6a89942a777a6eca3bc7d3442a8ed4f903438
daviz <daviz@daviz-laptop.(none)>
2013-06-12 23:38:00 +0200

    skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harbor::Application.config.secret_token = '2e847b87af742b3af8100eae1e1967000512f4d6950f0f8db7481bce904c9742c6db53c1ae3935a9cfb80b4022e6a1b411e68d30f0ea4cd50e7f202a0350e0b2'
